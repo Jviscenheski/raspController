5f7ec6fc39f97054be10ec841c71f5fdd1c1a66d
Rafael Ramos <rafael@datadriva.com>
2021-08-08 02:16:21 -0300

    Add: test

--- VoteDetection/src/real_time.py
+++ VoteDetection/src/real_time.py
@@ -18,29 +18,29 @@ while True:
     cap = cv2.VideoCapture(0)
     ret, frame = cap.read()
 
-    qr_data = qr_code_manager.readQrCode(frame)
+    # qr_data = qr_code_manager.readQrCode(frame)
+    #
+    # if qr_data == 'ops':
+    #     cap.release()
+    #     print('Está de meme? Voto de cabeça para baixo, pô')
+    #     continue
 
-    if qr_data == 'ops':
-        cap.release()
-        print('Está de meme? Voto de cabeça para baixo, pô')
-        continue
-
-    img, vote = vote_detector.executeDetectVotes(frame)
-
-    if isinstance(vote, list):
-        valid_votes.append(vote[0])
-    else:
-        valid_votes.append(vote)
-
-    if len(valid_votes) >= 10:
-        valid_vote = getMode(valid_votes)
-        print(valid_vote)
-        # COLOCAR no BANCO COM VALID_VOTE E QR_DATA
-        valid_votes = []
+    # img, vote = vote_detector.executeDetectVotes(frame)
+    #
+    # if isinstance(vote, list):
+    #     valid_votes.append(vote[0])
+    # else:
+    #     valid_votes.append(vote)
+    #
+    # if len(valid_votes) >= 10:
+    #     valid_vote = getMode(valid_votes)
+    #     print(valid_vote)
+    #     # COLOCAR no BANCO COM VALID_VOTE E QR_DATA
+    #     valid_votes = []
 
     # position = (150, 150)
     # cv2.putText(img, qr_data, position, cv2.FONT_HERSHEY_SIMPLEX, 5, (0, 255, 0, 255), 6)
-    # img = vote_detector.detectBar(img)
+    img = vote_detector.detectBar(frame)
 
     cv2.imshow('Pipa - Circle Detection', img)
     cap.release()
